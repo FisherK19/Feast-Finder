7953098d8bf35e05db8956203c77a405ff71f64e
kristie fisher <fisherkristie19@icloud.com>
2024-03-12 20:29:51 -0400

    added Emily's model folder and js files

---
+++ models/recipe.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+class Recipe extends Model {}
+Recipe.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    recipe_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    ingredients: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    instructions: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'recipe',
+  }
+);
+module.exports = Recipe;
