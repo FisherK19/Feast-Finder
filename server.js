7c6e4fa8bbce6d18e6044acb04622b7624cc750a
kristie fisher <fisherkristie19@icloud.com>
2024-03-14 18:21:10 -0400

    debugged login and register, they both render now, need to troubleshoot recipe

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+// Import required modules
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 
+// Create an instance of the Express application
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -20,6 +22,9 @@ app.use(express.json());
 // Middleware to parse URL-encoded bodies
 app.use(express.urlencoded({ extended: false }));
 
+// Middleware to serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Initialize express-session middleware and cookie-parser
 app.use(cookieParser());
 app.use(session({
@@ -29,22 +34,31 @@ app.use(session({
     cookie: { secure: true } 
 }));
 
-// Middleware to serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
-
 // Middleware for your controllers
 const userRoutes = require('./controllers/api/userRoutes');
 const recipeRoutes = require('./controllers/api/recipeRoutes'); 
 const loginRoute = require('./controllers/api/loginRoute');
 const registerRoute = require('./controllers/api/registerRoute');
-app.use('/recipes', recipeRoutes); 
-app.use('/users', userRoutes);
-app.use('/login', loginRoute);
-app.use('/register', registerRoute);
+// Route handler for the "/login" endpoint
+app.get('/login', (req, res) => {
+  res.render('login'); // Renders the login page
+});
+
+// Route handler for the "/register" endpoint
+app.get('/register', (req, res) => {
+  res.render('register'); 
+});
+
+// Route handler for the "/recipe" endpoint
+app.get('/recipe', (req, res) => {
+  res.render('recipe'); 
+});
+
+
 
 // Route handler for the root URL
 app.get('/', (req, res) => {
-    res.render('home'); // Assuming 'home.handlebars' is in the 'views' directory
+    res.render('home'); 
 });
 
 // Error handling middleware
