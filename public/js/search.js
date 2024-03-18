8a5fbe888a8fed6089046e4d2b8365cf611323ba
kristie fisher <fisherkristie19@icloud.com>
2024-03-18 14:58:57 -0400

    made changes in scipts and search

--- public/js/search.js
+++ public/js/search.js
@@ -1,36 +1,30 @@
-// Function to handle search form submission
 async function searchHandler(event) {
     event.preventDefault();
 
-    // Get the search query from the form input
     const searchQuery = document.getElementById('searchQuery').value;
 
     try {
-        // Send an AJAX request to the server to search for recipes
         const response = await fetch(`/recipes/search?query=${encodeURIComponent(searchQuery)}`);
         if (response.ok) {
-            // Parse the JSON response
             const data = await response.json();
             const recipes = data.recipes;
 
-            // Display the search results on the webpage
             const searchResultsElement = document.getElementById('searchResults');
-            searchResultsElement.innerHTML = ''; // Clear previous search results
+            searchResultsElement.innerHTML = '';
 
             if (recipes.length > 0) {
                 recipes.forEach(recipe => {
-                    // Create HTML elements to display each recipe
                     const recipeCard = document.createElement('div');
                     recipeCard.classList.add('recipe-card');
                     recipeCard.innerHTML = `
                         <h2>${recipe.recipeName}</h2>
+                        <img src="${recipe.imageUrl}" alt="Recipe Image">
                         <p><strong>Ingredients:</strong> ${recipe.ingredients}</p>
                         <p><strong>Directions:</strong> ${recipe.directions}</p>
                     `;
                     searchResultsElement.appendChild(recipeCard);
                 });
             } else {
-                // Display a message if no matching recipes found
                 searchResultsElement.innerHTML = '<p>No matching recipes found.</p>';
             }
         } else {
@@ -43,6 +37,4 @@ async function searchHandler(event) {
     }
 }
 
-    // Attach the event listener to handle search form submission
-    document.getElementById('searchForm').addEventListener('submit', searchHandler);
-
+document.getElementById('searchForm').addEventListener('submit', searchHandler);
